83d673e4ddefca892d2baa9f3e7e5fa96736a24f
Rohan Purekar <rohanpurekar1@gmail.com>
2021-09-28 16:51:08 +0530

    Removed the locations API

--- modules/locations_api/app/locations/controllers.py
+++ modules/locations_api/app/locations/controllers.py
@@ -17,16 +17,10 @@ api = Namespace("locations", description="Connections via geolocation.")  # noqa
 
 
 
-@api.route("/locations")
+# @api.route("/locations")
 @api.route("/locations/<location_id>")
 @api.param("location_id", "Unique ID for a given Location", _in="query")
 class LocationResource(Resource):
-    @accepts(schema=LocationSchema)
-    @responds(schema=LocationSchema)
-    def post(self) -> Location:
-        request.get_json()
-        location: Location = LocationService.create(request.get_json())
-        return location
 
     @responds(schema=LocationSchema)
     def get(self, location_id) -> Location:
