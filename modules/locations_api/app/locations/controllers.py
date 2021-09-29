09d1306d535f15eca403fbf747af7f4c4c9b1397
Rohan Purekar <rohanpurekar1@gmail.com>
2021-09-29 19:45:19 +0530

    Added post requests methods

--- modules/locations_api/app/locations/controllers.py
+++ modules/locations_api/app/locations/controllers.py
@@ -6,11 +6,10 @@ from app.locations.schemas import (
     LocationSchema,
 )
 from app.locations.services import LocationService
-from flask import request, g
+from flask import request, g, Response
 from flask_accepts import accepts, responds
 from flask_restx import Namespace, Resource
 from typing import Optional, List
-from kafka import KafkaProducer
 
 DATE_FORMAT = "%Y-%m-%d"
 
@@ -23,11 +22,11 @@ api = Namespace("locations", description="Connections via geolocation.")  # noqa
 class LocationResource(Resource):
     @responds(schema=LocationSchema)
     def get(self, location_id) -> Location:
-        TOPIC_NAME = 'location_api'
-        KAFKA_SERVER = 'my-release-kafka-0.my-release-kafka-headless.default.svc.cluster.local:9092'
-        producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
-        message=dict({'Name':'Rohan Purekar', 'status':'Alright!'})
-        producer.send(TOPIC_NAME, bytes(str(message), 'utf-8'))
-        producer.flush()
         location: Location = LocationService.retrieve(location_id)
         return location
+    
+    @accepts(schema=LocationSchema)
+    def post(self) -> Location:
+        request.get_json()
+        LocationService.push_message_into_queue(request.get_json())
+        return Response(status=202)
--- modules/locations_api/app/locations/services.py
+++ modules/locations_api/app/locations/services.py
@@ -7,6 +7,7 @@ from app.locations.models import Connection, Location
 from app.locations.schemas import ConnectionSchema, LocationSchema
 from geoalchemy2.functions import ST_AsText, ST_Point
 from sqlalchemy.sql import text
+from kafka import KafkaProducer
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("udaconnect-api")
@@ -26,3 +27,22 @@ class LocationService:
         # Rely on database to return text form of point to reduce overhead of conversion in app code
         location.wkt_shape = coord_text
         return location
+
+    @staticmethod
+    def push_message_into_queue(location: Dict) -> Location:
+        validation_results: Dict = LocationSchema().validate(location)
+        if validation_results:
+            logger.warning(f"Unexpected data format in payload: {validation_results}")
+            raise Exception(f"Invalid payload: {validation_results}")
+
+        new_location = Location()
+        new_location.person_id = location["person_id"]
+        new_location.creation_time = location["creation_time"]
+        new_location.coordinate = ST_Point(location["latitude"], location["longitude"])
+
+        TOPIC_NAME = 'location_api'
+        KAFKA_SERVER = 'my-release-kafka-0.my-release-kafka-headless.default.svc.cluster.local:9092'
+        producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+        
+        producer.send(TOPIC_NAME, bytes(str(new_location), 'utf-8'))
+        producer.flush()
