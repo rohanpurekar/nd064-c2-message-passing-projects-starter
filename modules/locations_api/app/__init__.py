fb70442ae3ab3a297132e93ac5561f382b590ae3
Rohan Purekar <rohanpurekar1@gmail.com>
2021-09-29 20:20:49 +0530

    Creating Broker connection during app init

--- modules/locations_api/app/__init__.py
+++ modules/locations_api/app/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g
 from flask_cors import CORS
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
+from kafka import KafkaProducer
 
 db = SQLAlchemy()
 
@@ -17,6 +18,13 @@ def create_app(env=None):
 
     register_routes(api, app)
     db.init_app(app)
+
+    @app.before_first_request
+    def setup_kafka_connections():
+        g.TOPIC_NAME = 'location_api'
+        KAFKA_SERVER = 'my-release-kafka-0.my-release-kafka-headless.default.svc.cluster.local:9092'
+        producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+        g.kafka_producer = producer
 
     @app.route("/health")
     def health():
--- modules/locations_api/app/locations/services.py
+++ modules/locations_api/app/locations/services.py
@@ -1,13 +1,13 @@
 import logging
 from datetime import datetime, timedelta
 from typing import Dict, List
-
+from flask import g
 from app import db
 from app.locations.models import Connection, Location
 from app.locations.schemas import ConnectionSchema, LocationSchema
 from geoalchemy2.functions import ST_AsText, ST_Point
 from sqlalchemy.sql import text
-from kafka import KafkaProducer
+
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("udaconnect-api")
@@ -34,15 +34,7 @@ class LocationService:
         if validation_results:
             logger.warning(f"Unexpected data format in payload: {validation_results}")
             raise Exception(f"Invalid payload: {validation_results}")
-
-        # new_location = Location()
-        # new_location.person_id = location["person_id"]
-        # new_location.creation_time = location["creation_time"]
-        # new_location.coordinate = ST_Point(location["latitude"], location["longitude"])
-
-        TOPIC_NAME = 'location_api'
-        KAFKA_SERVER = 'my-release-kafka-0.my-release-kafka-headless.default.svc.cluster.local:9092'
-        producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
         
-        producer.send(TOPIC_NAME, bytes(str(location), 'utf-8'))
-        producer.flush()
+        location_producer = g.kafka_producer
+        location_producer.send(g.TOPIC_NAME, bytes(str(location), 'utf-8'))
+        location_producer.flush()
