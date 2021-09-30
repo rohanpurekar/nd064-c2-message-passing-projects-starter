2f822b695499479ffdf4e742fbf2ef746e36668f
Rohan Purekar <rohanpurekar1@gmail.com>
2021-09-30 10:33:51 +0530

    Added a tuple notation on topics

--- modules/kafka-consumer/consumer.py
+++ modules/kafka-consumer/consumer.py
@@ -5,7 +5,7 @@ PERSONS_TOPIC_NAME = 'person_api'
 LOCATIONS_TOPIC_NAME = 'location_api'
 KAFKA_SERVER = 'my-release-kafka-0.my-release-kafka-headless.default.svc.cluster.local:9092'
 # location_producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
-consumer = KafkaConsumer([PERSONS_TOPIC_NAME, LOCATIONS_TOPIC_NAME], bootstrap_servers=KAFKA_SERVER, value_deserializer=lambda m: json.dumps(m.decode('utf-8')))
+consumer = KafkaConsumer(topics=([PERSONS_TOPIC_NAME, LOCATIONS_TOPIC_NAME]), bootstrap_servers=KAFKA_SERVER, value_deserializer=lambda m: json.dumps(m.decode('utf-8')))
 
 for message in consumer:
     print(message)
