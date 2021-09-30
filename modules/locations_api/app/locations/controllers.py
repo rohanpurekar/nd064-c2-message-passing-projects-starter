27a7e7f21227050a513d5445280418fc660f54bb
Rohan Purekar <rohanpurekar1@gmail.com>
2021-09-30 17:44:21 +0530

    Updated the API name and added docs

--- modules/locations_api/app/locations/controllers.py
+++ modules/locations_api/app/locations/controllers.py
@@ -17,6 +17,14 @@ api = Namespace("locations", description="Connections via geolocation.")  # noqa
 
 
 @api.route("/locations")
+class LocationResource(Resource):
+    @accepts(schema=LocationSchema)
+    def post(self) -> Location:
+        request.get_json()
+        LocationService.push_message_into_queue(request.get_json())
+        return Response(status=202)
+
+
 @api.route("/locations/<location_id>")
 @api.param("location_id", "Unique ID for a given Location", _in="query")
 class LocationResource(Resource):
@@ -24,9 +32,3 @@ class LocationResource(Resource):
     def get(self, location_id) -> Location:
         location: Location = LocationService.retrieve(location_id)
         return location
-    
-    @accepts(schema=LocationSchema)
-    def post(self) -> Location:
-        request.get_json()
-        LocationService.push_message_into_queue(request.get_json())
-        return Response(status=202)
